57a2f9ab13e78f9caa6dc039a262327b7d125481
Julian <jschaefe@stud.hs-offenburg.de>
2022-04-04 14:16:02 +0200

    Rotate Script

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+   public float speed;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward *speed * Time.deltaTime);    
+        
+    }
+}
